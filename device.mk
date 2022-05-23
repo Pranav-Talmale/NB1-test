bc4c880c69c4cc412618dcc792dd36c51f24f144
Kevin Wilms <niemandausduisburg@gmx.net>
2022-05-23 15:33:47 +0200

    Revert "NB1: Bump to AOSP Keymaster 4.0"
    This reverts commit 8004e2d94a17b034bb93642cafa7810f8ba4815a.

--- device.mk
+++ device.mk
@@ -288,9 +288,9 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0.vendor \
-    android.hardware.keymaster@4.0-impl \
-    android.hardware.keymaster@4.0-service
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
